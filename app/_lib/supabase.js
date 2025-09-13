c5ff50695a4f4faf800c014f0907afc070d71804
joker733 <salmank786s@gmail.com>
2025-09-13 11:39:27 +0530

    first commit

---
+++ app/_lib/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key are required but missing.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
